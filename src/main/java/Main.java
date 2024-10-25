339e7133f88be8f463dcd088b8bbafd273dd8924
jibesh <jibeshshrestha00@gmail.com>
2024-10-25 19:17:33 +0545

    decode string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,40 +3,43 @@ import com.google.gson.Gson;
 import com.dampcake.bencode.Bencode;
 
 public class Main {
-  private static final Gson gson = new Gson();
+    private static final Gson gson = new Gson();
 
-  public static void main(String[] args) throws Exception {
-    String command = args[0];
-    if("decode".equals(command)) {
-      String bencodedValue = args[1];
-      Object decoded;
-      try {
-        decoded = decodeBencode(bencodedValue);
-      } catch(RuntimeException e) {
-        System.out.println(e.getMessage());
-        return;
-      }
-      System.out.println(gson.toJson(decoded));
+    public static void main(String[] args) throws Exception {
+        String command = args[0];
+        if("decode".equals(command)) {
+            String bencodedValue = args[1];
+            Object decoded;
+            try {
+                decoded = decodeBencode(bencodedValue);
+            } catch(RuntimeException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+            System.out.println(gson.toJson(decoded));
 
-    } else {
-      System.out.println("Unknown command: " + command);
+        } else {
+            System.out.println("Unknown command: " + command);
+        }
+
     }
 
-  }
+    static Object decodeBencode(String bencodedString) {
+        Bencode bencode = new Bencode();
+        char firstLetter = bencodedString.charAt(0);
+        if (Character.isDigit(firstLetter)) {
+            String result = bencode.decode(bencodedString.getBytes(), Type.STRING);
+            return result;
+        } else if (firstLetter == 'i') {
+            long result = bencode.decode(bencodedString.getBytes(), Type.NUMBER);
+            return result;
+        } else if (firstLetter == 'l') {
+            Object result = bencode.decode(bencodedString.getBytes(), Type.LIST);
+            return result;
+        }
+        else {
+            throw new RuntimeException("Bencode: " + bencodedString + " is not a digit");
+        }
+    }
 
-  static Object decodeBencode(String bencodedString) {
-    Bencode bencode = new Bencode();
-    char firstLetter = bencodedString.charAt(0);
-    if (Character.isDigit(firstLetter)) {
-      String result = bencode.decode(bencodedString.getBytes(), Type.STRING);
-      return result;
-    } else if (firstLetter == 'i') {
-      long result = bencode.decode(bencodedString.getBytes(), Type.NUMBER);
-      return result;
-    }
-    else {
-      throw new RuntimeException("Bencode: " + bencodedString + " is not a digit");
-    }
-  }
-  
 }
